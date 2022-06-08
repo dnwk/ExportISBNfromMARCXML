7ee01a8e48257215fa972967b24abe4cb2b303ac
Kun Lin <kunlin@usc.edu>
2022-06-08 11:22:30 -0700

    Add feature to zip the output

--- grabISBN.py
+++ grabISBN.py
@@ -1,12 +1,24 @@
+# /usr/bin/python3
+
 import sys
+import zipfile
 from datetime import datetime
 from pymarc import map_xml
 
+filedate = datetime.now().strftime("%Y%m%d")
+f = open("isbn"+filedate+".txt", "a")
+
 def print_isbn(r):
-    filedate = datetime.now().strftime("%Y%m%d")
-    f = open("isbn"+filedate+".txt", "a")
     tempISBN=r.isbn()
+    print('processing record '+tempISBN, end='\r')
     if tempISBN:
         f.write(tempISBN+"\n")
 
 map_xml(print_isbn, sys.argv[1])
+f.close()
+print('zipping records', end='\r')
+with zipfile.ZipFile("isbn"+filedate+".zip", "w") as myzip:
+    myzip.write("isbn"+filedate+".txt")
+myzip.close()
+print("Done")
+# End of grabISBN.py
