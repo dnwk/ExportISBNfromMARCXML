9f1c415c41a62025a8d49af9940da31433178796
Kun Lin <kunlin@usc.edu>
2022-06-08 10:18:08 -0700

    output ISBN only when it's not None

--- grabISBN.py
+++ grabISBN.py
@@ -5,6 +5,8 @@ from pymarc import map_xml
 def print_isbn(r):
     filedate = datetime.now().strftime("%Y%m%d")
     f = open("isbn"+filedate+".txt", "a")
-    f.write(r.isbn() + "\n")
+    tempISBN=r.isbn()
+    if tempISBN:
+        f.write(tempISBN+"\n")
 
 map_xml(print_isbn, sys.argv[1])
